6abc6cbea69b0c57afdb6c5b4cafb806e55161ba
Claes Jakobsson <claes@surfar.nu>
2021-12-24 22:13:41 +0100

    Generic JSON return

---
+++ json.go
@@ -0,0 +1,15 @@
+package serviceutils
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func SendJSON(res http.ResponseWriter, statusCode int, out interface{}) {
+	res.WriteHeader(statusCode)
+	err := json.NewEncoder(res).Encode(out)
+	if err != nil {
+		log.Println(err)
+	}
+}
