a4be9e44a2e9bd73eecc94104927e647a0687d11
Claes Jakobsson <claes@surfar.nu>
2022-01-10 21:45:08 +0100

    Added helper for gettings entities

--- entity.go
+++ entity.go
@@ -1,10 +1,29 @@
 package serviceutils
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type Entity struct {
-	Ok       bool   `json:"ok"`
-	Error    bool   `json:"error"`
-	ID       string `json:"id"`
-	Site     string `json:"site"`
-	Type     string `json:"type"`
-	ParentID string `json:"parent"`
+	Ok          bool   `json:"ok"`
+	Error       string `json:"error"`
+	ID          string `json:"id"`
+	Site        string `json:"site"`
+	Type        string `json:"type"`
+	Parent      string `json:"parent"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Location    string `json:"location"`
+	Preferences string `json:"preferences"`
+}
+
+func GetEntity(jwt string, site string, id string) (*Entity, int, error) {
+	var entity Entity
+	rs, err := GetJSON("entity-service", fmt.Sprintf("/v1/entity/%s/%s", site, id), nil, jwt, nil, &entity)
+	if rs != http.StatusOK {
+		return nil, rs, err
+	}
+
+	return &entity, rs, nil
 }
