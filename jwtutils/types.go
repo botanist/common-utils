8dccb0bd7849faccb42076af943a085c7e34f39e
Claes Jakobsson <claes@surfar.nu>
2022-05-16 22:58:32 +0200

    Added custom JWT type

--- jwtutils/types.go
+++ jwtutils/types.go
@@ -1,7 +1,14 @@
 package jwtutils
 
+import "github.com/cristalhq/jwt/v4"
+
 type Jwt struct {
 	OK    bool   `json:"ok"`
 	Error string `json:"error"`
 	Jwt   string `json:"jwt"`
+}
+
+type EntityClaims struct {
+	jwt.RegisteredClaims
+	Shard string `json:"sid,omitempty"`
 }
